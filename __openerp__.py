2ed3d69ae2138a583ae5e03553fe840df8882791
Timo Talvitie <timo.talvitie@vizucom.com>
2015-03-18 20:09:08 +0200

    Initial commit - add new workflow state to SOs

---
+++ __openerp__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   Copyright (c) 2013- Vizucom Oy (http://www.vizucom.com)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Finalization state for Sale Orders',
+    'category': 'Sales',
+    'version': '0.2',
+    'author': 'Vizucom Oy',
+    'website': 'http://www.vizucom.com',
+    'depends': ['sale_stock'],
+    'description': """
+Finalization state for Sale Orders
+==================================
+ * Adds a new state, 'Finalization', between Quotation Sent and Sale Order
+ * Intended for situations where the Quotations needs changes before it gets converted to Sale Order (e.g. the customer approves the quote but with slight changes). 
+ * NOTE: Depends on sale_stock so that the sale order states do not get overridden by it
+
+""",
+    'data': [
+        'view/sale_order.xml',
+        'workflow/sale_order_workflow.xml',
+    ],
+
+}
---
+++ sale_order.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from openerp.osv import osv, fields
+from openerp.tools.translate import _
+
+
+class sale_order(osv.Model):
+
+    _inherit = 'sale.order'
+    
+    fin_str = _('Finalization Phase')
+    
+    ''' When quotation is about to be set to "In finalization" '''
+    def action_finalization(self, cr, uid, ids, context=None):
+        return self.write(cr, uid, ids, {'state': 'finalization' }, context=context)
+
+    ''' Add new state '''
+    _columns = {
+        'state': fields.selection([
+            ('draft', 'Draft Quotation'),
+            ('sent', 'Quotation Sent'),
+            ('finalization', fin_str),
+            ('cancel', 'Cancelled'),
+            ('waiting_date', 'Waiting Schedule'),
+            ('progress', 'Sales Order'),
+            ('manual', 'Sale to Invoice'),
+            ('invoice_except', 'Invoice Exception'),
+            ('done', 'Done'),
+            ], 'Status', readonly=True, track_visibility='onchange',
+            help="Gives the status of the quotation or sales order. \nThe exception status is automatically set when a cancel operation occurs in the processing of a document linked to the sales order. \nThe 'Waiting Schedule' status is set when the invoice is confirmed but waiting for the scheduler to run on the order date.", select=True),
+    }
